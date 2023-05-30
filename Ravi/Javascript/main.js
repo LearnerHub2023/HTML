88041089a09b37b77bd4ad89f806c38c5de6f222
Frontend <skilsfrom2018@gmail.com>
2023-05-30 12:19:36 +0530

    javascript basics 2

--- Ravi/Javascript/main.js
+++ Ravi/Javascript/main.js
@@ -17,3 +17,27 @@ document.querySelector('#bluboff').addEventListener("click",function(){
     // blub on function code 
     document.querySelector('#blub').src='pic_bulboff.gif';
 });
+
+document
+  .querySelector("#borderChangeBtn")
+  .addEventListener("click", function () {
+    document.querySelector("#square").style.border = "2px solid red";
+    document.querySelector("#square").style.backgroundColor = "blue";
+  });
+
+  document
+  .querySelector("#show")
+  .addEventListener("click", function () {
+    document.querySelector("#square").style.display = "block";
+  });
+
+  document
+  .querySelector("#hide")
+  .addEventListener("click", function () {
+    document.querySelector("#square").style.display = "none";
+    window.alert("hello");
+    alert("hi");
+    console.log("testing");
+    document.write("testing");
+  });
+
