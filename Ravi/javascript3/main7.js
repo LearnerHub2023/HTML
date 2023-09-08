8bc867be1850e77ecd6c460e9dbeffcad9068397
Frontend <skilsfrom2018@gmail.com>
2023-09-08 12:21:12 +0530

    while do while and expressions

---
+++ Ravi/javascript3/main7.js
@@ -0,0 +1,32 @@
+// while loop
+
+// initialiation;
+// while (condition){
+// our code
+// increement;
+// }
+
+let arrayofnumbers= [100, 200, 500, 400, 300, 600, 800, 900];
+
+let i = 0;
+while (i < arrayofnumbers.length) {
+  let x = arrayofnumbers[i];
+    console.log(x);
+  i++;
+}
+
+
+//initialization
+// do{
+//   our code
+//  increment
+// }
+// while(condition)
+
+let j = 0;
+do{
+    let x = arrayofnumbers[j];
+    console.log(x);
+    j++;
+}
+while (false);
---
+++ Ravi/javascript3/main8.js
@@ -0,0 +1,17 @@
+// (a+b)2 = a2+ b2 +2ab
+// (a+b)^2 = a^2 +b^2 +2 * a * b
+
+let expression;
+let a = 0;
+let b= 20;
+
+expression = (a**2) + (b**2) + (2 * a * b); //arithmetic exp
+ 
+expression = a < b; //boolean expression <, >, (<=, >=, ==), (===, <==, >==)
+
+expression = a && b;   // ||, &&, !
+
+// expression = a;
+// let a;
+
+console.log(expression);
